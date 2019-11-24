328a93dc1f7e288eabb6cf561bb7f184255ad304
tysonlander <tysonlander@gmail.com>
2019-11-23 21:58:12 -0700

    Sessions

---
+++ website4/page4.php
@@ -0,0 +1,10 @@
+<?php 
+  session_start();
+
+  session_destroy();
+  
+  // print_r($_SESSION);
+  // $name = isset($_SESSION['name']) ?  $_SESSION['name'] : 'Guest';
+  // $email = $_SESSION['email'];
+
+  
